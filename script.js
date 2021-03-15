c37dcd407e45182192018efc6d4983d0d4658a6a
HoYean Lee <ghdusjang8@gmail.com>
2021-03-15 19:01:08 +0900

    할 일 추가/완료 기능 구현

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-// Hello World!
+// load element
+const waitLabel = document.getElementById('wait_label');
+const doneLabel = document.getElementById('done_label');
+const waitList = document.getElementById('wait_list_container');
+const doneList = document.getElementById('done_list_container');
+const addButton = document.getElementById('btn_add');
+const input = document.getElementById('input_todo');
+
+const list = [];
+
+refreshList();
+
+addButton.onclick = function () {
+  list.push({
+    text: input.value,
+    isDone: false,
+  });
+
+  refreshList();
+  input.value = '';
+  return false;
+};
+
+function refreshList() {
+  doneList.innerHTML = '';
+  waitList.innerHTML = '';
+  let waitCount = 0;
+  let doneCount = 0;
+
+  list.forEach((element, index) => {
+    if (element.isDone) {
+      doneList.innerHTML +=
+          `<li onclick="setDone(${index}, false)">${element.text}</li>`;
+      doneCount++;
+    } else {
+      waitList.innerHTML +=
+        `<li onclick="setDone(${index}, true)">${element.text}</li>`;
+      waitCount++;
+    }
+  });
+
+  waitLabel.innerHTML = '대기중 (' + waitCount + ')';
+  doneLabel.innerHTML = '완료됨 (' + doneCount + ')';
+}
+
+function setDone(id, value) {
+  list[id].isDone = value;
+  console.log(list[id]);
+  refreshList();
+}
