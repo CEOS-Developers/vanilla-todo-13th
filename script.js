d3124fbfaa7bed89cc31b591027ad2696fcf8792
Bum <sbl133@naver.com>
2021-03-16 15:34:51 +0900

    initallist

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+const toDoList = document.querySelector(".toDoList");
+const toDoForm = document.querySelector(".toDoForm");
+const toDoInput = toDoForm.querySelector("input");
 
-//123123
-////123124124
-//123123
-//setstset//setstsetse
-//testesets
-123l;k12jlk;dsjaflk
-asdasd
-asdasd
-asdasd
-testset
+
+const appendList = (value) => {
+    const li = document.createElement("li");
+    const delButton = document.createElement("button");
+    delButton.innerHTML = "<img src='./img/bin.png' width='18'>";
+    const text = document.createElement("span");
+    text.innerText = value;
+    li.appendChild(text);
+    li.appendChild(delButton);
+    toDoList.appendChild(li);
+}
+const submitHandler = (event) => {
+    event.preventDefault();
+    const value = toDoInput.value;
+    appendList(value);
+    toDoInput.value = "";
+}
+const init = () =>{
+    toDoForm.addEventListener("submit", submitHandler);
+}
+
+init();
