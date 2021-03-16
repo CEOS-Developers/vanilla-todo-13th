d1ef0fc1c2388ffcf46a2e1265762a4057e4c37d
Bum <sbl133@naver.com>
2021-03-16 18:40:25 +0900

    toggle

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
 const toDoList = document.querySelector(".toDoList");
 const toDoForm = document.querySelector(".toDoForm");
 const toDoInput = toDoForm.querySelector("input");
+const comList = document.querySelector(".completeList");
 
-//localstorage에 올릴 배열할당
+// 전체 list 관리위한 배열할당
 let toDos = [];
-
+// localstorage에 올림
 const saveLocalStorage = () => {
     localStorage.setItem("toDos", JSON.stringify(toDos));
 }
-const delHnadler = (event) => {
+// delButton의 click handler
+const delHandler = (event) => {
     const btn = event.target.parentNode;
     const li = btn.parentNode;
-    toDoList.removeChild(li);
+    if (li.parentNode.className === "toDoList") {
+        toDoList.removeChild(li);
+    } else {
+        comList.removeChild(li);
+    }
     const deletedList = toDos.filter((v) => {
-        console.log(parseInt(li.id) !== v.id)
         return parseInt(li.id) !== v.id;
     });
-    console.log(deletedList);
     toDos = deletedList;
     saveLocalStorage();
+}
+// list의 click handler
+const toggleHandler = (e) => {
+    if (e.target && e.target.nodeName === "LI"){
+            const li = e.target;
+        const index = toDos.findIndex((v) => v.id === parseInt(li.id));
+        if (toDos[index].com === false) {
+            toDos[index].com = true;
+            comList.appendChild(li);
+        } else if (toDos[index].com === true) {
+            toDos[index].com = false;
+            toDoList.appendChild(li);
+        }
+    }
 }
 // list추가
 const appendList = (value) => {
     const li = document.createElement("li");
     const delButton = document.createElement("button");
-    const text = document.createElement("span");
     delButton.innerHTML = "<img src='./img/bin.png' width='18'>";
-    delButton.addEventListener("click", delHnadler);
+    li.addEventListener("click", toggleHandler);
+    delButton.addEventListener("click", delHandler);
     const newId = toDos.length + 1;
     li.id = newId;
-    text.innerText = value;
-    li.appendChild(text);
+    li.innerText = value;
     li.appendChild(delButton);
+    console.log("appendList에 li : ", li);
     toDoList.appendChild(li);
-    // localstorage에 올릴 새 list에 해당하는 객체 생성
+    // 새로 생성된 list에 해당하는 객체 생성
     const toDoObj = {
         text: value,
         id: newId,
+        com: false,
     }
     toDos.push(toDoObj);
     saveLocalStorage();
