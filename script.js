7c31f8d94f10104482e1aaff1ff816c60d6adf29
SojeongLee <sojunglsj@naver.com>
2021-03-18 21:31:46 +0900

    add list event

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
+const form = document.querySelector(".input-box"),
+    input=form.querySelector("input"),
+    waitList=document.querySelector(".wait-list");
 
-// commit test
+const TODO_LS='toDos';
+
+function loadToDos(){
+    const toDos=localStorage.getItem(TODO_LS);
+    if(toDos!==null){
+
+    }
+}
+
+function paintToDo(text){
+    const plusLi = document.createElement('li');
+    plusLi.innerHTML = input.value;
+    waitList.append(plusLi);   
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue=input.value;
+    paintToDo(currentValue);
+    input.value="";
+}
+
+function init(){
+    loadToDos();
+    form.addEventListener("submit",handleSubmit);
+}
+
+init();
