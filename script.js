b5292c0d9f2f027b3269adbd8a65571ada94547e
Yunsun Jung <99yunsun@naver.com>
2021-03-19 23:28:22 +0900

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-hello world!
-dldldldlldl
+function addToDo() {
+    const toDoValue = document.getElementById("new-to-do").value;
+    if (toDoValue != '') {
+        let newItem = document.createElement('div');
+        newItem.className = 'todo';
+        
+        let toDoSpan = document.createElement('span');
+        let toDoValueSpan = document.createTextNode(toDoValue);
+        toDoSpan.appendChild(toDoValueSpan);
+        newItem.appendChild(toDoSpan);
+        
+        let trashImg = document.createElement('img');
+        trashImg.src = "img/bin.png";
+        trashImg.className = "trashcan";
+        newItem.appendChild(trashImg);
+        
+        document.getElementById("content-incomplete").appendChild(newItem);
+        document.getElementById("new-to-do").value = '';
+    }
+}
+
+function handleKeyDown(e) {
+    // console.log(e.code);
+    if (e.code == "Enter") {
+        addToDo();
+    }
+}
+
+function initializeApp() {
+    document.getElementById('new-to-do').addEventListener('keydown', handleKeyDown);
+
+}
+
+window.onload = initializeApp;
 
