34837611982f8717fd074681217c2978a16fce92
SJKim <hedro0458@gmail.com>
2021-03-19 16:25:48 +0900

    [FEAT] imply deleting todo event

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function deleteTodoEvent(event) {
+    let targetEvent = event.target.parentNode;
+    targetEvent.parentNode.removeChild(targetEvent);
+    console.log('deleted')
+}
+
 function addTodoEvent(event) {
     let content = document.querySelector("#todo-input--input").value;
 
@@ -19,11 +25,15 @@ function addTodoEvent(event) {
 
     let todoListCount = document.querySelectorAll(".todolist-waits--content").length;
     document.querySelector("#todolist-waits--count").innerHTML = todoListCount;
+    document.querySelectorAll('.todolist-delete-button').forEach(function (element) {
+        element.addEventListener('click', deleteTodoEvent);
+    })
 
-    
     // Prevent reloading when submit
     event.preventDefault();
-
 }
 
 document.querySelector('#todo-input').addEventListener('submit', addTodoEvent);
+document.querySelectorAll('.todolist-delete-button').forEach(function (element) {
+    element.addEventListener('click', deleteTodoEvent);
+})
