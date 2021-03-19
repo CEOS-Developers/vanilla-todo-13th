0f961f5e0a3726b5d26f793be5350859318a14a1
SojeongLee <sojunglsj@naver.com>
2021-03-19 09:20:06 +0900

    something so strange

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const form = document.querySelector(".input-box"),
 
 const TODO_LS='toDos';
 
-
-
 function loadToDos(){
     const loadedToDos=localStorage.getItem(TODO_LS);
     if(loadedToDos!==null){
@@ -25,26 +23,25 @@ function paintToDo(text){
     const plusLi = document.createElement('li');
     const delBtn = document.createElement('img');
     
-   
-    
-    //const img=document.createElement("img");
-    delBtn.src="img/bin.png";
-    //delBtn.innerHTML="$";
+    const span = document.createElement("span");
 
-    //delBtn.append(img);
+    
+    const img=document.createElement("img");
+    delBtn.src="img/bin.png";
+    
     delBtn.addEventListener("click",deleteToDo);
     const newId=toDos.length+1;
-    //plusLi.innerHTML = input.value;
+    plusLi.innerText = input.value;
     const inputText=document.createTextNode(input.value);
-    plusLi.id=newId;
+    plusLi.id="plusLi";
+   
+    //plusLi.addEventListener("click", toggleToDo);
     
-    const span = document.createElement("span");
-    span.addEventListener("click", toggleToDo);
-    //span.innerText=text;
     span.appendChild(inputText);
+    span.appendChild(delBtn);
+    plusLi.addEventListener("click", toggleToDo);
+    //plusLi.appendChild(delBtn);
     plusLi.appendChild(span);
-    plusLi.appendChild(delBtn);
-    
     waitList.appendChild(plusLi);
 
     const toDoObj = {
@@ -59,9 +56,25 @@ function toggleToDo(event){
     console.log("toggle");
     const btn = event.target;
     const li = btn.parentNode;
-    //if(waitList.has)
-    completeList.append(li);
-    //waitList.removeChild(li);
+    const span = document.createElement("span");
+    const delBtn=document.createElement('img');
+    delBtn.src="img/bin.png";
+    const inputText=document.createTextNode(li.innerText);
+    
+    while(li.hasChildNodes()){
+        li.removeChild(li.firstChild);
+    }
+   
+    delBtn.addEventListener("click",deleteComplete);
+    span.addEventListener("click", toggleUp);
+    
+    span.appendChild(inputText);
+    li.appendChild(span);
+    li.after(delBtn);
+    
+    completeList.appendChild(li);
+
+    
 }
 
 function deleteToDo(event){
@@ -69,6 +82,39 @@ function deleteToDo(event){
     const btn = event.target;
     const li = btn.parentNode;
     waitList.removeChild(li);
+    completeList.removeChild(li);
+}
+
+function toggleUp(event){
+    const btn=event.target;
+    const li=btn.parentNode;
+    const span = document.createElement("span");
+    const delBtn=document.createElement('img');
+    delBtn.src="img/bin.png";
+    /*
+    li.removeEventListener("click",toggleUp);
+    li.addEventListener("click", function(){
+        completeList.remove(li);
+        waitList.append(li);
+        li.addEventListener("click", toggleToDo);
+    });
+    */
+    
+    while(li.hasChildNodes()){
+        li.removeChild(li.firstChild);
+    }
+    delBtn.addEventListener("click",deleteToDo);
+    span.addEventListener("click", toggleToDo);
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    waitList.append(li);
+
+}
+
+function deleteComplete(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    completeList.removeChild(li);
 }
 
 function handleSubmit(event){
